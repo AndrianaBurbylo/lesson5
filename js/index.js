b25f138b6ca559b6ad9e8f192009de3c13605845
Andriana Burbylo <a.burbylo1512@gmail.com>
2018-03-29 10:15:49 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const a = parseFloat(prompt('Enter A'));
+const b = parseFloat(prompt('Enter B'));
+const c = parseFloat(prompt('Enter C'));
+
+const result = solveQE(a, b, c);
+document.write(result);
+
+function solveQE(a, b, c){
+    const d = calcD(a, b, c);
+    if (d>0){
+        const x1 = (-b + Math.sqrt(d))/2*a;
+        const x2 = (-b - Math.sqrt(d))/2*a;
+        return 'x1 = ' + x1 + ', x2 = ' + x2;
+    }
+    else if (d<0) {
+        return 'no solutions';
+    }
+    else {
+        return 'x = ' + (-b/2*a);
+    }
+}
+
+function calcD(a, b, c){
+    return b**2 - 4*a*c;
+}
+
+
+
+
+
+
